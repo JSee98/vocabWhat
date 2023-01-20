7fa5f2254da9d931d75dbe180748a58d1e9eae63
JSee98 <40459836+JSee98@users.noreply.github.com>
2023-01-20 18:03:25 +0530

    minor fix

---
+++ vocab-what-api/migrations/1673111160274-migration.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class migration1673111160274 implements MigrationInterface {
+    name = 'migration1673111160274'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "user_entity" ("id" SERIAL NOT NULL, "email" character varying NOT NULL, "lastLogin" TIMESTAMP, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_b54f8ea623b17094db7667d8206" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "user_entity"`);
+    }
+
+}
